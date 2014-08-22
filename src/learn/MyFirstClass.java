1485babf40b0115bd0797b47b884a48d140bc5e9
sodhibhupinder <sodhi.bhupinder@gmail.com>
2014-08-22 09:51:09 +0100

    hello world!

---
+++ src/learn/MyFirstClass.java
@@ -0,0 +1,27 @@
+/*
+ * @MyFirstClass.java	@22 Aug 2014
+ *
+ * Copyright (c) 2014 TESCO. 
+ * All rights reserved. 
+ * 
+ * No part of this document may be reproduced or transmitted in any form or by 
+ * any means, electronic or mechanical, whether now known or later invented, 
+ * for any purpose without the prior and express written consent of TESCO 
+ * 
+ */
+package learn;
+
+/**
+ * @author KP84
+ * @22 Aug 2014
+ */
+public class MyFirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
